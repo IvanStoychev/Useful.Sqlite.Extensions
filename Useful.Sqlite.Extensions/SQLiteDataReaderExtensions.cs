74a6dca68e6b6e3acbc2cee817bab700baf13c6c
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-02-28 18:00:51 +0200

    Added "GetTimeSpanTicksFromLong" method.
    Updated summaries.

--- Useful.Sqlite.Extensions/SQLiteDataReaderExtensions.cs
+++ Useful.Sqlite.Extensions/SQLiteDataReaderExtensions.cs
@@ -5,16 +5,43 @@ namespace Useful.Sqlite.Extensions
 {
     public static class SQLiteDataReaderExtensions
     {
+        #region GetTimeSpan
+        /// <summary>
+        /// Gets the value of the column, with the given number, as a string and
+        /// parses it as a TimeSpan.
+        /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
+        /// <param name="columnNumber">The number of the column whose data to parse.</param>
+        /// <returns>A TimeSpan representation of the column value.</returns>
         public static TimeSpan GetTimeSpanFromString(this SQLiteDataReader reader, int columnNumber)
         {
             string timeSpanString = reader.GetString(columnNumber);
-            TimeSpan result = 
+            TimeSpan result = TimeSpan.Parse(timeSpanString);
+
+            return result;
         }
+
+        /// <summary>
+        /// Returns a TimeSpan object with a number of ticks equal to the value of the column
+        /// with the given number.
+        /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
+        /// <param name="columnNumber">The number of the column whose data to parse.</param>
+        /// <returns>A TimeSpan with the column value as the number of ticks.</returns>
+        public static TimeSpan GetTimeSpanTicksFromLong(this SQLiteDataReader reader, int columnNumber)
+        {
+            long ticks = reader.GetInt64(columnNumber);
+            TimeSpan result = new TimeSpan(ticks);
+
+            return result;
+        }
+        #endregion GetTimeSpan
 
         #region GetDateTime
         /// <summary>
         /// Parses the value of the column, with the given number, as a DateTime using the provided format.
         /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
         /// <param name="columnNumber">The number of the column whose data to parse.</param>
         /// <param name="format">A format specifier that defines how the column's value will be parsed to a DateTime.</param>
         /// <returns>A DateTime representation of the column value.</returns>
@@ -29,6 +56,7 @@ namespace Useful.Sqlite.Extensions
         /// <summary>
         /// Parses the value of the column, with the given number, as a DateTime using the provided format specifiers.
         /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
         /// <param name="columnNumber">The number of the column whose data to parse.</param>
         /// <param name="format">A format specifier that defines how the column's value will be parsed to a DateTime.</param>
         /// <param name="formatProvider">An object that supplies culture-specific format information about the column's value.</param>
@@ -47,6 +75,7 @@ namespace Useful.Sqlite.Extensions
         /// Checks if the value of the given column number is null. If it is - returns null,
         /// otherwise parses it as a DateTime.
         /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
         /// <param name="columnNumber">The number of the column whose data to get.</param>
         /// <returns>"null" if the column has no value or its value as a DateTime.</returns>
         public static DateTime? GetDateTimeNullable(this SQLiteDataReader reader, int columnNumber)
@@ -61,6 +90,7 @@ namespace Useful.Sqlite.Extensions
         /// Checks if the value of the given column number is null. If it is - returns null,
         /// otherwise parses it as a DateTime using the provided format.
         /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
         /// <param name="columnNumber">The number of the column whose data to get.</param>
         /// <param name="format">A format specifier that defines how the column's value will be parsed to a DateTime.</param>
         /// <returns>"null" if the column has no value or its value as a DateTime.</returns>
@@ -78,6 +108,7 @@ namespace Useful.Sqlite.Extensions
         /// Checks if the value of the given column number is null. If it is - returns null,
         /// otherwise parses it as a DateTime, using the provided date format and IFormatProvider.
         /// </summary>
+        /// <param name="reader">The <see cref="SQLiteDataReader"/> that will read the data from the database.</param>
         /// <param name="columnNumber">The number of the column whose data to get.</param>
         /// <param name="format">A format specifier that defines how the column's value will be parsed to a DateTime.</param>
         /// <param name="formatProvider">An object that supplies culture-specific format information about the column's value.</param>
